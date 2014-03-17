01fae89e94395ec8898d47996b8aaaf54c1fdf40
Sean Feil <sean@com.tophatandmonocle>
2014-03-17 11:32:10 -0600

    Deuglification of mobile/links

--- application/views/test/layout.php
+++ application/views/test/layout.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>HD Window Cleaning</title>
+    <meta name="description" content="HD Window Cleaning is a professional window cleaning service for Calgary and area. We do residential (home) and commercial (high rise) window cleaning.">
+    <title>HD Window Cleaning - Calgary and Area</title>
 
     <link href="<?=base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?=base_url() ?>assets/css/styles.css" rel="stylesheet">
@@ -15,6 +16,11 @@
     <![endif]-->
   </head>
   <body>
+
+    <div id="mobilemenu">
+      <div id="menuanchor"><a href="#">Menu <span class="glyphicon glyphicon-th-list"></span></a></div>
+    </div>
+
     <div class="container" id="maincontent">
       <div class="row">
         <div class="col-sm-4" id="logo">
@@ -23,19 +29,19 @@
         <div class="col-sm-2"></div>
         <div class="col-sm-2 toplink" id="quote">
           <div class="navicon"><span class="glyphicon glyphicon-ok"></span></div>
-          <a href="#">Why Us?</a>
+          <span class="ctalink"><a href="#">Why Us?</a></span>
           <p class="clinger">Learn what makes us special</p>
         </div>
 
         <div class="col-sm-2 toplink borderleft" id="book">
           <div class="navicon"><span class="glyphicon glyphicon-fire"></span></div>
-          <a href="#">Get Started</a>
+          <span class="ctalink"><a href="#">Get Started</a></span>
           <p class="clinger">Get a free quote and book an appointment</p>
         </div>
 
         <div class="col-sm-2 toplink borderleft" id="quote">
           <div class="navicon"><span class="glyphicon glyphicon-bell"></span></div>
-          <a href="#">Contact Us</a>
+          <span class="ctalink"><a href="#">Contact Us</a></span>
           <p class="clinger">Questions? Let us know!</p>
         </div>
 
@@ -50,14 +56,34 @@
 
     </div>
 
+    <div class="container" id="linkscontainer">
+      <div class="row">
+        <div class="col-sm-2 aboutuslink col-sm-offset-1" id="rservices">
+          <a href="#"><span class="aboutuslinka">Residential Services</span></a>
+        </div>
+        <div class="col-sm-2 aboutuslink" id="cservices">
+          <a href="#"><span class="aboutuslinka">Commercial Services</span></a>
+        </div>
+        <div class="col-sm-2 aboutuslink" id="servicearea">
+          <a href="#"><span class="aboutuslinka">Service Area</span></a>
+        </div>
+        <div class="col-sm-2 aboutuslink" id="guarantee">
+          <a href="#"><span class="aboutuslinka">Our Guarantee to You</span>
+        </div>
+        <div class="col-sm-2 aboutuslink" id="Quote">
+          <a href="#"><span class="aboutuslinka">Get an Instant Quote</a></span>
+        </div>
+      </div>
+    </div>
+
+
     <div class="container" id="whatwedo">
       <h1 id="pitchhead">Beautify Your View</h1>
 
-      <p>Locavore jean shorts Tumblr cardigan Shoreditch. Brooklyn hella Vice wolf trust fund plaid semiotics fixie authentic keytar, beard High Life ugh. Banksy forage PBR&B aesthetic before they sold out raw denim. Single-origin coffee selfies DIY mustache freegan. IPhone Shoreditch paleo drinking vinegar direct trade gluten-free, disrupt aesthetic chambray typewriter selfies Bushwick master cleanse locavore squid. Vegan umami Neutra retro street art, beard fingerstache pop-up. Meh VHS fanny pack, try-hard stumptown McSweeney's irony 8-bit biodiesel master cleanse Wes Anderson.</p>
-
-      <p>Fap aesthetic freegan master cleanse trust fund Cosby sweater. Shabby chic sartorial McSweeney's literally scenester, kogi tofu tote bag 90's gentrify narwhal. Deep v Thundercats brunch direct trade. Bicycle rights synth skateboard, tote bag fap cardigan distillery mlkshk whatever. Thundercats kale chips vegan distillery, next level DIY biodiesel Helvetica flannel Tumblr American Apparel food truck slow-carb. American Apparel sriracha High Life, literally four loko Brooklyn direct trade hashtag. Biodiesel fanny pack bespoke Banksy VHS.</p>
+      <p>Locavore jean shorts Tumblr cardigan Shoreditch. Brooklyn hella Vice wolf trust fund plaid semiotics fixie authentic keytar, beard High Life ugh. Banksy forage PBR&B aesthetic before they sold out raw denim. Single-origin coffee selfies DIY mustache freegan. IPhone Shoreditch paleo drinking vinegar direct trade gluten-free, disrupt aesthetic</p>
 
     </div>
+
 
 
     <script src="<?=base_url() ?>assets/js/jquery.min.js"></script>
