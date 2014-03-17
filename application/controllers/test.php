cd0deeff43df485a00e153fa93a68b4fd7e6b958
Sean Feil <sean@com.tophatandmonocle>
2014-03-17 00:08:42 -0600

    Initial design testing.

---
+++ application/controllers/test.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Test extends MY_Controller {
+
+	public function index()
+	{
+           
+	}
+
+	public function layout()
+	{
+		$this->loadview('test/layout');
+	}
+}
---
+++ application/views/test/layout.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HD Window Cleaning</title>
+
+    <link href="<?=base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?=base_url() ?>assets/css/styles.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container" id="maincontent">
+      <div class="row">
+        <div class="col-sm-4" id="logo">
+          <img src="<?=base_url() ?>assets/img/badlogo.png" alt="Bad Logo">
+        </div>
+        <div class="col-sm-2"></div>
+        <div class="col-sm-2 toplink" id="quote">
+          <a href="#">Get a Online Quote</a>
+        </div>
+
+        <div class="col-sm-2 toplink" id="book">
+          <a href="#">Book an Appointment</a>
+        </div>
+
+        <div class="col-sm-2 toplink" id="quote">
+          <a href="#">Contact Us</a>
+        </div>
+
+      </div>
+
+
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="<?=base_url() ?>assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
