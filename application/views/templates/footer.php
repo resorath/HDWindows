ce8a9154668af3f696e82c951e09a71a79db0486
Sean Feil <sean@com.tophatandmonocle>
2014-03-24 22:18:01 -0600

    Add tracking code

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -9,7 +9,16 @@
       </ul>
     </div>
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+      ga('create', 'UA-49342770-1', 'hdwindows.ca');
+      ga('send', 'pageview');
+
+    </script>
     <script src="<?=base_url() ?>assets/js/modernizr.js"></script>
     <script src="<?=base_url() ?>assets/js/jquery.min.js"></script>
     <script src="<?=base_url() ?>assets/js/bootstrap.min.js"></script>
