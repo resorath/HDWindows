51ffbb83978ff223d5626ab681c2a9033dfb7f89
Sean Feil <sean@com.tophatandmonocle>
2014-04-06 17:51:31 -0600

    Undo rotation check

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,35 +12,6 @@ $(document).ready(function(){
 
 });
 
-// prevent rotation
-$(window).bind('orientationchange resize', function(event){
-  if (event.orientation) {
-    if (event.orientation == 'landscape') {
-      if (window.rotation == 90) {
-        rotate(this, -90);
-      } else {
-        rotate(this, 90);
-      }
-    }
-  }
-});
-
-function rotate(el, degs) {
-  iedegs = degs/90;
-  if (iedegs < 0) iedegs += 4;
-  transform = 'rotate('+degs+'deg)';
-  iefilter = 'progid:DXImageTransform.Microsoft.BasicImage(rotation='+iedegs+')';
-  styles = {
-    transform: transform,
-    '-webkit-transform': transform,
-    '-moz-transform': transform,
-    '-o-transform': transform,
-    filter: iefilter,
-    '-ms-filter': iefilter
-  };
-  $(el).css(styles);
-}
-// end
 
 // Scrolling navigation
 $('.scrolllink').click(function() {
