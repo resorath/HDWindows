35edc46b44d8a097d8aa3fc3c200971b18315e64
Sean Feil <sean@tophatandmonocle.com>
2014-09-16 21:04:20 -0600

    casing

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,7 +1,7 @@
 <div class="weakcontainer dashboard">
 	<div id="admin-topbar">
 		<span id="admin-top-left">Hello <?=$_SESSION['user'] ?>, Welcome to HDWindows Administrative Panel (HDAP).</span>
-		<span id="admin-logout"><a href="<?=base_url() ?>/Admin/Logout">Log Out</a> | <a href="#">Change Password</a></span>
+		<span id="admin-logout"><a href="<?=base_url() ?>/admin/Logout">Log Out</a> | <a href="#">Change Password</a></span>
 	</div>
 
 	<div class="row" id="admin-dashboardrow">
